5f811675a79ff728a0cf1c51ccd7c73c858ff091
john nicholas <themrtortoise@gmail.com>
2017-01-21 13:11:31 -0800

    initial commit p2

---
+++ lib/elixir_ninety_nine_problems.ex
@@ -0,0 +1,18 @@
+defmodule ElixirNinetyNineProblems do
+  @moduledoc """
+  Documentation for ElixirNinetyNineProblems.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNinetyNineProblems.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_ninety_nine_problems_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNinetyNineProblemsTest do
+  use ExUnit.Case
+  doctest ElixirNinetyNineProblems
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
