b5e5ea5e84c97014cda13b21da57d4f379d18e32
John Nicholas <themrtortoise@gmail.com>
2017-03-07 13:08:30 +0000

    beginning to work on tree approach, experiment with detecting start of new list in tail

--- lib/find_permutations_in_list.ex
+++ lib/find_permutations_in_list.ex
@@ -12,13 +12,40 @@ def flatten([h|t]), do: [h] ++ flatten([t])
 
 # each route starts from a leaf and creates a list of all parents
 # this list needs to be gotten for every leaf of the tree
-def expand([]), do: []
-def expand([x]), do: [[x]]
-def expand([x,[h|t]]), do: [getPathToLeaf([],[x,[h|t]])]
+def expandTree(list), do: expandTree([],[],list)
+# def expand([x,[h|t]]), do: getPathToLeaf([],[x,[h|t]],[])
 
-def getPathToLeaf(parents,[h|[]]), do: parents ++ [h]
-def getPathToLeaf(parents,[h|[t]]), do: getPathToLeaf(parents ++ [h],t)
+defp expandTree(acc, parents, []), do: acc ++ [[parents]]
+defp expandTree(acc, parents, [item]) when not is_list(item), do: acc ++ [parents ++ [item]]
+defp expandTree(acc, parents, [h|[t]]) when is_list(t), do: expandTree(acc, parents++[h],t)
 
+
+
+# def accumulatePathsToLeaves(parentNodes, accumulator, []), do: accumulator ++ [[parentNodes]]
+# def accumulatePathsToLeaves(parentNodes, accumulator, [h|[item]]), do: accumulatePathsToLeaves(parentNodes ++ [h], accumulator, [item])
+# def accumulatePathsToLeaves(parentNodes, accumulator, [item]), do: accumulator ++ [parentNodes ++ item]
+# def accumulatePathsToLeaves(parentNodes, accumulator, []), do: accumulator ++ [parentNodes]
+# def accumulatePathsToLeaves(parentNodes, accumulator, [h|[t]]) when is_list(t), do: accumulatePathsToLeaves(parentNodes, accumulator, t)
+# def accumulatePathsToLeaves(parentNodes, accumulator, [h|t]) do
+#   accumulatePathsToLeaves(
+#   parentNodes ++ [h],
+#   accumulatePathsToLeaves(parentNodes,accumulator,t),
+#   getStartOfNextList(t)  )
+# end
+
+#if node has tail that is not start of a list then branch(parents, accumulator, pass in tail)
+#if node has a tail that is the start of a list then create new level(parents ++ this node, accumulator, pass in tail)
+
+
+def getStartOfNextList([]), do: []
+def getStartOfNextList([_|[t]]) when is_list(t), do: t
+def getStartOfNextList([_|t]), do: getStartOfNextList(t)
+# def accumulatePathsToLeaves(parentNodes, accumulator,[this|[children]]),
+# def accumulatePathsToLeaves(parentNodes,accumulator,[this|children]), do: accumulatePathsToLeaves(parentNodes++[this],accumulator,children)
+
+# def getPathToLeaf(parents,[h|[t]],acc), do: acc ++ [getPathToLeaf(parents ++ [h],t)]
+# def getPathToLeaf(parents,[item]), do: parents ++ [item]
+# def getPathToLeaf(parents,item), do: parents ++ [item]
 
 
 
--- test/find_permutations_in_list_test.exs
+++ test/find_permutations_in_list_test.exs
@@ -19,19 +19,34 @@ end
 # end
 
 test "asserts expand 1 item" do
-  assert FindPermutationsInList.expand([1]) == [[1]]
+  assert FindPermutationsInList.expandTree([1]) == [[1]]
 end
 
 test "assert expand 2 item" do
-  assert FindPermutationsInList.expand([1,[2]]) == [[1,2]]
+  assert FindPermutationsInList.expandTree([1,[2]]) == [[1,2]]
 end
 
 # test "assert expand 3 items" do
 #   assert FindPermutationsInList.expand([1,[2,[3]]]) == [[1,2,3]]
 # end
 #
-# test "assert expand 2 items, second list" do
-#   assert FindPermutationsInList.expand([1,[2,3]]) == [[1,2],[1,3]]
-# end
+test "assert expand 2 items, second list" do
+  assert FindPermutationsInList.expandTree([1,[2,3]]) == [[1,2],[1,3]]
+end
 
+test "assert get start of next list for empty list" do
+  assert FindPermutationsInList.getStartOfNextList([]) == []
+end
+
+test "assert get start of next list fort no next list is empty" do
+  assert FindPermutationsInList.getStartOfNextList([1,2,3]) == []
+end
+
+test "assert get start of next list works for list with sub list" do
+  assert FindPermutationsInList.getStartOfNextList([1,2,[3]]) == [3]
+end
+
+test "assert get start of list for multiple sublists returns correctly" do
+  assert FindPermutationsInList.getStartOfNextList([1,2,[3,[4,[5,6]]]])  == [3,[4,[5,6]]]
+end
 end
