687c567f6bdadaaf7bd6d6a511d07f065911d7ff
John Nicholas <themrtortoise@gmail.com>
2017-03-07 23:14:30 +0000

    still long way to go.

--- test/find_permutations_in_list_test.exs
+++ test/find_permutations_in_list_test.exs
@@ -18,6 +18,8 @@ test "assert 3 item list gets all" do
   assert FindPermutationsInList.permute([1,2,3]) == [[1, [2, [3]], [3, [2]]], [2, [1, [3]], [3, [1]]], [3, [1, [2]], [2, [1]]]]
 end
 
+
+
 test "asserts expand 1 item" do
   assert FindPermutationsInList.expandTree([1]) == [[1]]
 end
@@ -32,6 +34,10 @@ end
 
 test "assert expand 2 items, second list" do
   assert FindPermutationsInList.expandTree([1,[2,3]]) == [[1,2],[1,3]]
+end
+
+test "unpack something with 2 parts" do
+  assert FindPermutationsInList.expandTree([[1, [2, [3]], [3, [2]]]]) == [[1,2,3],[1,3,2]]
 end
 
 
